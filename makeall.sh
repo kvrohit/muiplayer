3a74960d0d4ddeef76c076709c51a242ec1ce9ba
ksk.saikrishna <ksk.saikrishna@49fcfdf2-c925-11dd-a2ab-b939405fd53f>
2008-12-20 17:16:44 +0000

    Updated script for linux_lib

--- makeall.sh
+++ makeall.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-echo -e "--- building project MUI ---\n"
+echo -e "--- Building MUI ---\n"
 
-echo -e "--- building cfmod library ---\n"
+echo -e "--- Building cfmod library ---\n"
 cd src/cfmod
-make -f makefile.linux
+make linux_lib
 if [ $? -ne 0 ]
 then
-    echo -e "--- error building cfmod ---\n"
+    echo -e "--- Error building cfmod ---\n"
     exit
 fi
 echo -e "--- cfmod library built ---\n"
+
+echo -e "--- Building cplaylist library ---\n"
+cd ../cplaylist
+make linux_lib
+
+if[ $? -ne 0]
+then
+	echo -e "--- Error building cplaylist ---\n"
+	exit
+fi
+echo -e "--- cplaylist library built ---\n"
 
 echo -e "--- building MUI ---\n"
 cd ../MUI
