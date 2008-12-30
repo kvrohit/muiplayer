00c3e84d2813ff05d8f2cf26eeabfc2f628117e2
Rohit K Viswanath <kvrohit@gmail.com>
2008-12-30 15:21:35 +0000

    small change to album art display

--- src/MUI/sources/metadatawidget.cpp
+++ src/MUI/sources/metadatawidget.cpp
@@ -8,11 +8,13 @@ MetaDataWidget::MetaDataWidget(QWidget *parent) : QWidget(parent)
 void MetaDataWidget::setTag(AudioTag::GenericTag tag)
 {
     QPixmap qp;
+    qp.load(":/images/images/noart.jpg");
     
     ui.labelTitle->setText(tag.title.c_str());
     ui.labelArtist->setText(tag.artist.c_str());
     ui.labelAlbum->setText(tag.album.c_str());
     ui.labelYear->setText(QString("%1").arg(tag.year));
+    ui.labelAlbumArt->setPixmap(qp);
     
     if(tag.artfile != "")
     {           
