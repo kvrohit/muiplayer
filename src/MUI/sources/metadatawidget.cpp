771a3c13b187332f5fa66203685beb2090d0e21c
ksk.saikrishna <ksk.saikrishna@49fcfdf2-c925-11dd-a2ab-b939405fd53f>
2009-01-03 14:01:24 +0000

    MUI displays Unicode tags correctly now

--- src/MUI/sources/metadatawidget.cpp
+++ src/MUI/sources/metadatawidget.cpp
@@ -10,10 +10,10 @@ void MetaDataWidget::setTag(AudioTag::GenericTag tag)
     QPixmap qp;
     qp.load(":/images/images/noart.jpg");
     
-    ui.labelTitle->setText(tag.title.c_str());
-    ui.labelArtist->setText(tag.artist.c_str());
-    ui.labelAlbum->setText(tag.album.c_str());
-    ui.labelYear->setText(QString("%1").arg(tag.year));
+    ui.labelTitle->setText(convertToUnicode(tag.title.c_str()));
+    ui.labelArtist->setText(convertToUnicode(tag.artist.c_str()));
+    ui.labelAlbum->setText(convertToUnicode(tag.album.c_str()));
+    ui.labelYear->setText( tag.year ? QString("%1").arg(tag.year) : QString("Unknown") );
     ui.labelAlbumArt->setPixmap(qp);
     
     if(tag.artfile != "")
---
+++ src/MUI/sources/support.h
@@ -0,0 +1,14 @@
+/**
+
+Support functions for MUI
+
+*/
+
+#ifndef __SUPPORT_H
+#define __SUPPORT_H
+
+#include<QString>
+
+QString convertToUnicode(std::string);
+
+#endif /* __SUPPORT_H */
