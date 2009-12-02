fbf995b21ca6a2ab3b25309441d0416fe95f57b3
ksk.saikrishna <ksk.saikrishna@49fcfdf2-c925-11dd-a2ab-b939405fd53f>
2009-12-02 16:23:53 +0000

    Added fectching of album art from folder feature. Looks for a file named "cover.jpg" for album art.

--- trunk/src/MUI/sources/metadatawidget.h
+++ trunk/src/MUI/sources/metadatawidget.h
@@ -3,6 +3,9 @@
 
 #include <QWidget>
 #include <QPixmap>
+#include <QtGui>
+#include <iostream>
+#include <cstddef>
 #include "ui_metadata.h"
 #include "caudiotags.hpp"
 
@@ -13,7 +16,7 @@ class MetaDataWidget : public QWidget
     Q_OBJECT
     public:
         MetaDataWidget(QWidget *parent = 0);
-        void setTag(AudioTag::GenericTag);
+        void setTag(AudioTag::GenericTag, std::string);
         
     private:
         Ui::Metadata ui;
