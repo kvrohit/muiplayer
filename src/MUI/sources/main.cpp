25717f59e7583eaeff9d1692a2b47d7ead30bc0b
Rohit K Viswanath <kvrohit@gmail.com>
2011-06-03 11:46:23 +0000

    * Added library search path so that Qt can load additional plugins

--- src/MUI/sources/main.cpp
+++ src/MUI/sources/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setApplicationName("MUI");
+    a.addLibraryPath(QCoreApplication::applicationDirPath());
     MUI w;
     w.setWindowIcon(QIcon(Mui::MediaPlaybackIcon));
     w.show();
